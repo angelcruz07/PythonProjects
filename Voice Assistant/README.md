653c20406e9ede65c8f93c0ea7ac0b2989397ccc
Angel Cruz <angelcruzsanchez126@gmail.com>
2023-09-26 22:03:59 -0600

    Voice assistant in python, improvable

---
+++ Voice Assistant/README.md
@@ -0,0 +1,24 @@
+## Voice assistant
+
+## Description
+
+My most recent project in python
+
+A voice assistant previously loaded
+with the libraries.
+
+* pyttsx3
+* speech_recognition
+* re
+
+
+Making use of regular expressions as bot response parameters
+
+# Initiation
+
+Install the corresponding
+libraries and run the script in 
+the conosole or developement enviroment
+
+
+Thank you for reading me!!!.
---
+++ Voice Assistant/voice_assistant.py
@@ -0,0 +1,57 @@
+import pyttsx3
+import speech_recognition as sr
+import re
+
+def inicialize_engine():
+  
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 120)
+    engine.setProperty("voice", "spanish")
+    
+    return engine
+
+
+def recognizer_voice(r):
+  
+   with sr.Microphone() as source:
+        print("Puedes hablar")
+        audio = r.listen(source)
+        text = r.recognize_google(audio, language="es-ES")
+        name = identify_name(text)  
+        
+   return text
+        
+        
+def identify_name(text):
+  name = None
+  patterns = ["me llamo ([A-Za-z]+)", "mi nombre es ([A-Za-z]+)", "^([A-Za-z]+)$"]
+  for pattern in patterns:
+      try:
+        name = re.findall(pattern, text)[0]
+      except IndexError:
+        pass
+  return name
+     
+    
+
+def main():
+  
+    engine = inizialize_engine()
+    
+    engine.say("Hola como te llamas")
+    engine.runAndWait()
+
+    r = sr.Recognizer()
+
+    text = recognizer_voice()
+    name = identify_name(text)
+   
+    if name:
+        engine.say("Encantado de conocerte, {}".format(name))
+    else:
+          engine.say("Pues mira la verdad que no te entiendo")
+    engine.runAndWait()
+            
+        
+if __name__ == "__main__":
+    main()
